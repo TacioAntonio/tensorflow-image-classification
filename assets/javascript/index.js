4be78b428241b3760a8ab7e442b63456b2b0bab7
pomania <pomaniammorpg@gmail.com>
2022-08-01 15:14:40 -0300

    feat: created image classification

---
+++ assets/javascript/index.js
@@ -0,0 +1,37 @@
+let net;
+
+const UPLOAD = document.querySelector('#animal');
+const IMG =  document.querySelector('.card__image');
+const LIST = document.querySelector('.card__list');
+
+UPLOAD.addEventListener('change', async function(event) {
+    const { target } = event;
+    const blobFile = target?.files[0],
+          nameFile = blobFile?.name;
+
+    const reader = new FileReader();
+
+    const readerLoadend =  () => {
+        IMG.src = reader.result?.toString();
+        IMG.value = nameFile;
+        
+        reader.removeEventListener('loadend', readerLoadend);
+    }
+
+    reader.addEventListener('loadend', readerLoadend);
+    reader.readAsDataURL(blobFile);
+
+    net = await mobilenet.load();
+    const result = await net.classify(IMG);
+    handleList(result)
+    IMG.value = '';
+});
+
+
+function handleList(result) {
+    LIST.innerHTML = '';
+    
+    result.forEach(({ className, probability }) => {
+        LIST.innerHTML += `<li class="card__item">Breed: ${className} - Probility: ${probability}</li>`;
+    });
+}
